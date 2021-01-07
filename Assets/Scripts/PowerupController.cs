4971683620bfb5c77f5c68e8632658c8fac8f747
STEEDCLINT ALEX <steed@unist.ac.kr>
2021-01-07 13:34:12 +0900

    Shield working.

---
+++ Assets/Scripts/PowerupController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PowerupController :MonoBehaviour
+{
+    #region Field Declarations
+
+    public GameObject explosion;
+
+    [SerializeField]
+    private PowerType powerType;
+
+    #endregion
+
+    #region Movement
+
+    void Update()
+    {
+       Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * 3, Space.World);
+
+        if (ScreenBounds.OutOfBounds(transform.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    #region Collisons
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+	    //Assume we are colliding with a player
+	    if (powerType == PowerType.Shield)
+	    {
+	    	PlayerController player = FindObjectOfType<PlayerController>();
+	    	player.EnableShield();
+	    }
+	    
+       
+       Destroy(gameObject);
+    }
+
+    #endregion
+}
+
+public enum PowerType
+{
+    Shield,
+    X2
+};
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    #region Field Declarations
+
+    public Vector2 projectileDirection;
+    public float projectileSpeed;
+    public bool isPlayers;
+
+    #endregion
+
+    #region Movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveProjectile();
+    }
+
+    private void MoveProjectile()
+    {
+        transform.Translate(projectileDirection * Time.deltaTime * projectileSpeed, Space.World);
+
+        if (ScreenBounds.OutOfBounds(transform.position))
+        {
+        	if (isPlayers)
+        	{
+        		PlayerController player = FindObjectOfType<PlayerController>();
+        		player.EnableProjectile();
+        	}
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
